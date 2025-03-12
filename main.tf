b63c9524f77abc0e192aaa2d21820965afffe6f6
ritviksaxena <78605818+ritviksaxena4@users.noreply.github.com>
2025-03-12 21:36:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "newbucket" {
   count = 2
 }
+
+
+resource "aws_instance" "new-instance" {
+  instance_type = "t2.micro"
+  key_name = "terraformkp1"
+  ami = "ami-04b4f1a9cf54c11d0"
+}
